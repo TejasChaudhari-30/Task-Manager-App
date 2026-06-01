672bfec0254af4153bee8dcf1a371475959cb8e7
Tejas Chaudhari <tejaschaudhari@gmail.com>
2026-06-01 16:30:14 +0530

    Added Vercel URL to CORS

--- Backend/src/middleware/corsmiddleware.js
+++ Backend/src/middleware/corsmiddleware.js
@@ -3,7 +3,7 @@ import cors from "cors";
 export const corsmiddleware= cors({
     origin: [
       "http://localhost:5173",       // dev
-      "https://task-manager-app-lemon-five.vercel.app/",            // prod
+      "https://task-manager-app-lemon-five.vercel.app",            // prod
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
